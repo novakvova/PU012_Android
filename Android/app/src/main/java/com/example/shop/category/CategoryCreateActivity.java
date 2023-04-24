af3997e540c3e152692617f372cf4ae5384eead3
novakvova <novakvova@gmail>
2023-04-24 10:01:49 +0300

    Begin working menu

---
+++ Android/app/src/main/java/com/example/shop/category/CategoryCreateActivity.java
@@ -0,0 +1,17 @@
+package com.example.shop.category;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shop.BaseActivity;
+import com.example.shop.R;
+
+public class CategoryCreateActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_create);
+    }
+}
